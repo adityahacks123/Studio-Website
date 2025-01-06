ea8e07747e6bb85d6a408ee5924c73a0667c3c6a
adityahacks123 <adityasingh30112020@gmail.com>
2025-01-06 19:38:18 +0530

    container created for image

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+ var elemC=document.querySelector("#elem-container")
+ var fixed=document.querySelector("#fixed-image")
+ elemC.addEventListener("mouseenter", function(){
+fixed.style.display="block"
+ })
+
+ elemC.addEventListener("mouseleave", function(){
+    fixed.style.display="none"
+     })
